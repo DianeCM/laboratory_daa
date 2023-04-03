2ee5c36056ee5248fc60c449eb147bfed42aea22
Jordan Pla <jordanpg41@gmail.com>
2023-04-03 16:18:30 -0700

    initial solution

---
+++ count_backtrack.py
@@ -0,0 +1,21 @@
+def count_backtrack(T:str, A:str):
+    if 0 < len(T) and len(T) <= len(A):
+        def backtrack(T:list, A:list, index:int, S:list):
+            if len(A) == index:
+                if T == S[0:len(T)]:return 1
+                return 0
+    
+            char = A[index]
+            cant = backtrack(T, A, index+1, char+S)
+            cant += backtrack(T, A, index+1, S+char)
+            return cant
+        return backtrack(T, A, 0, "")
+    return 0
+
+print(count_backtrack('ACAC','ACACBCA'))
+print(count_backtrack('ACAC','CACA'))
+print(count_backtrack('ACABD','ACABDBCACA'))
+print(count_backtrack('DBACA', 'ACABD'))
+print(count_backtrack('AABBAA','BABAAA'))
+print(count_backtrack('AABBAA','CHAABBAA'))
+print(count_backtrack('ACAB','BDACACBA'))
